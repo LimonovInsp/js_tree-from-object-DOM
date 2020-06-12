ac8d744781536e02f53e298fc44e2c1ca9f247d8
LimonovInsp <bis.limonov@gmail.com>
2020-06-12 18:11:29 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  const list = document.createElement('ul');
+
+  for (const key in data) {
+    const point = document.createElement('li');
+
+    point.textContent = key;
+
+    createTree(point, data[key]);
+
+    list.append(point);
+  }
+
+  element.append(list);
+}
+
+createTree(tree, food);
